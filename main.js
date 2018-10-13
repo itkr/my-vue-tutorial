199a8bc37915d72d255131d269e58bcddb43d7cd
itkr <1531860+itkr@users.noreply.github.com>
2018-10-13 20:56:39 +0900

    JSON

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Spam!',
+        aList: [],
         spamList: [
             {
                 id: 1,
@@ -12,7 +13,7 @@ var app = new Vue({
                 s: 'b'
             },
             {
-                id: 2,
+                id: 3,
                 s: 'c'
             }
         ],
@@ -28,9 +29,18 @@ var app = new Vue({
         handleClick: function(e) {
             this.show = !this.show
             this.counter += 1
+            this.spamList.push({
+                id: 3 + this.counter,
+                s: 'test'
+            })
         }
     },
     created: function() {
         console.log('created')
+        axios.get('list.json').then(function(response) {
+            this.aList = response.data
+        }.bind(this)).catch(function(e) {
+            console.error(e)
+        })
     }
 })
